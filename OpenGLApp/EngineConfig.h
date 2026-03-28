b67e0dc3c1e07e9a8de1454885a442da696cf375
zinzin-101 <96015391+zinzin-101@users.noreply.github.com>
2026-03-28 22:38:44 +0700

    Update EngineConfig.h

--- OpenGLApp/EngineConfig.h
+++ OpenGLApp/EngineConfig.h
@@ -4,7 +4,7 @@
 namespace EngineConfig {
 	constexpr unsigned int DEFAULT_SCREEN_WIDTH = 1600;
 	constexpr unsigned int DEFAULT_SCREEN_HEIGHT = 900;
-	constexpr bool INIT_IN_FULL_SCREEN = true;
+	constexpr bool INIT_IN_FULL_SCREEN = false;
 	inline const char* WINDOW_NAME = "ZEngine";
 	constexpr SceneManagerConfig::SceneId INITIAL_SCENE_ID = SceneManagerConfig::SOFT_BODY_TEST_SCENE;
 	constexpr float FIXED_DT = 1.0f / 144.0f;
